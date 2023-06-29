f64108f9be7adb5a5b4c7bff6318af0033572a1f
Paula <58055615+apaulag@users.noreply.github.com>
2023-06-29 18:46:28 +0000

    Update triangle.ts

--- triangle.ts
+++ triangle.ts
@@ -1,27 +1,40 @@
 export class Triangle {
   constructor(...sides: number[]) {
+    this.sides = sides;
+  }
+
+  get isTriangle(): boolean {
     // Side lengths should be positive numbers greater than zero*.
-    const validSideCount: number = sides.filter((side) => side > 0).length;
+    const validSideCount: number = this.sides.filter((side) => side > 0).length;
     // A triangle should have exactly 3 sides of valid length.
-    let isTriangle: boolean = sides.length === 3 && validSideCount === 3;
-    // The number of different side length determines the type of the triangle.
-    const distinctSideCount: number = new Set(sides).size;
-
-    // Inequality Test: a^2 + b^2 >= c^2 for any sides a, b, c.
-    const [a, b, c] = sides;
-    if (
-      Math.pow(a, 2) + Math.pow(b, 2) < Math.pow(c, 2) ||
-      Math.pow(b, 2) + Math.pow(c, 2) < Math.pow(a, 2) ||
-      Math.pow(c, 2) + Math.pow(a, 2) < Math.pow(b, 2)
-    ) {
-      isTriangle = false;
+    if (this.sides.length !== 3 || validSideCount !== 3) {
+      return false;
     }
+    // Inequality Test: a^2 + b^2 >= c^2 for any sides a, b, c.
+    const [a, b, c] = this.sides;
+    return (
+      Math.pow(a, 2) + Math.pow(b, 2) >= Math.pow(c, 2) &&
+      Math.pow(b, 2) + Math.pow(c, 2) >= Math.pow(a, 2) &&
+      Math.pow(c, 2) + Math.pow(a, 2) >= Math.pow(b, 2)
+    );
+  }
 
+  get distinctSideCount(): number {
+    return new Set(this.sides).size;
+  }
+
+  get isEquilateral(): boolean {
     // An equilateral triangle has all three sides the same length.
-    this.isEquilateral = isTriangle && distinctSideCount === 1;
+    return this.isTriangle && this.distinctSideCount === 1;
+  }
+
+  get isIsosceles(): boolean {
     // An isosceles triangle has at least two sides the same length.
-    this.isIsosceles = isTriangle && distinctSideCount <= 2;
+    return this.isTriangle && this.distinctSideCount <= 2;
+  }
+
+  get isScalene(): boolean {
     // A scalene triangle has all sides of different lengths.
-    this.isScalene = isTriangle && distinctSideCount === 3;
+    return this.isTriangle && this.distinctSideCount === 3;
   }
 }
