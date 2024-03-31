0f4abd717220f179803f33522a5dcabe94c50904
apaulag <58055615+apaulag@users.noreply.github.com>
2024-03-30 22:49:05 -0500

    Update triangle.ts

--- triangle.ts
+++ triangle.ts
@@ -1,39 +1,37 @@
 export class Triangle {
+  private readonly sides: number[];
+  
   constructor(...sides: number[]) {
     this.sides = sides;
   }
 
-  get isTriangle(): boolean {
+  public get isTriangle(): boolean {
     // Side lengths should be positive numbers greater than zero*.
     const validSideCount: number = this.sides.filter((side) => side > 0).length;
     // A triangle should have exactly 3 sides of valid length.
     if (this.sides.length !== 3 || validSideCount !== 3) {
       return false;
     }
-    // Inequality Test: a^2 + b^2 >= c^2 for any sides a, b, c.
+    // Inequality Test: a + b >= c for any side length a, b, c.
     const [a, b, c] = this.sides;
-    return (
-      Math.pow(a, 2) + Math.pow(b, 2) >= Math.pow(c, 2) &&
-      Math.pow(b, 2) + Math.pow(c, 2) >= Math.pow(a, 2) &&
-      Math.pow(c, 2) + Math.pow(a, 2) >= Math.pow(b, 2)
-    );
+    return (a + b >= c && b + c >= a && c + a >= b);
   }
 
-  get distinctSideCount(): number {
+  private get distinctSideCount(): number {
     return new Set(this.sides).size;
   }
 
-  get isEquilateral(): boolean {
+  public get isEquilateral(): boolean {
     // An equilateral triangle has all three sides the same length.
     return this.isTriangle && this.distinctSideCount === 1;
   }
 
-  get isIsosceles(): boolean {
+  public get isIsosceles(): boolean {
     // An isosceles triangle has at least two sides the same length.
     return this.isTriangle && this.distinctSideCount <= 2;
   }
 
-  get isScalene(): boolean {
+  public get isScalene(): boolean {
     // A scalene triangle has all sides of different lengths.
     return this.isTriangle && this.distinctSideCount === 3;
   }
